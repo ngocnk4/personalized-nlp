7b78fbadfc1308a654ac60dc6115b755ad0b4a4a
Wojciech Korczyński <wojtek19962a32@gmail.com>
2022-03-22 15:25:36 +0100

    feat: add past embedding model

--- personalized_nlp/models/__init__.py
+++ personalized_nlp/models/__init__.py
@@ -1,18 +1,19 @@
-
 from personalized_nlp.models.baseline import Net
 from personalized_nlp.models.onehot import NetOneHot
 from personalized_nlp.models.human_bias import HumanBiasNet
 from personalized_nlp.models.bias import AnnotatorBiasNet
+from personalized_nlp.models.past_embeddings import PastEmbeddingsNet
 from personalized_nlp.models.word_bias import WordBiasNet
 from personalized_nlp.models.annotator import AnnotatorEmbeddingNet
 from personalized_nlp.models.annotator_word import AnnotatorWordEmbeddingNet
 
 models = {
     'baseline': Net,
-    'onehot': NetOneHot, 
-    'peb': HumanBiasNet, 
+    'onehot': NetOneHot,
+    'peb': HumanBiasNet,
     'word_bias': WordBiasNet,
-    'bias': AnnotatorBiasNet, 
-    'embedding': AnnotatorEmbeddingNet, 
-    'word_embedding': AnnotatorWordEmbeddingNet
+    'bias': AnnotatorBiasNet,
+    'embedding': AnnotatorEmbeddingNet,
+    'word_embedding': AnnotatorWordEmbeddingNet,
+    'past_embeddings': PastEmbeddingsNet
 }
