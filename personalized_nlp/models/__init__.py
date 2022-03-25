e3e02172f9332d5028ea28836f733596293514a8
Wojciech Korczyński <wojtek19962a32@gmail.com>
2022-03-25 09:06:58 +0100

    feat: add transformer baseline experiments

--- personalized_nlp/models/__init__.py
+++ personalized_nlp/models/__init__.py
@@ -3,6 +3,7 @@ from personalized_nlp.models.onehot import NetOneHot
 from personalized_nlp.models.human_bias import HumanBiasNet
 from personalized_nlp.models.bias import AnnotatorBiasNet
 from personalized_nlp.models.past_embeddings import PastEmbeddingsNet
+from personalized_nlp.models.transformer_baseline import TransformerBaseline
 from personalized_nlp.models.word_bias import WordBiasNet
 from personalized_nlp.models.annotator import AnnotatorEmbeddingNet
 from personalized_nlp.models.annotator_word import AnnotatorWordEmbeddingNet
@@ -15,5 +16,6 @@ models = {
     'bias': AnnotatorBiasNet,
     'embedding': AnnotatorEmbeddingNet,
     'word_embedding': AnnotatorWordEmbeddingNet,
-    'past_embeddings': PastEmbeddingsNet
+    'past_embeddings': PastEmbeddingsNet,
+    'transformer_baseline': TransformerBaseline
 }
